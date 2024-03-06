d8b641d53cf417cc701ef6b4eb5a6f5a94f9cca3
Nurhaliza12 <lizanazila3@gmail.com>
2024-03-07 01:29:44 +0700

    jawab soal

--- problem2/main.py
+++ problem2/main.py
@@ -4,3 +4,5 @@ tinggi = 25.0
 
 
 # kode disini
+Luas = alas * tinggi/2
+print (Luas)
--- problem4/main.py
+++ problem4/main.py
@@ -3,3 +3,10 @@ Input Harga: 370000
 Input Diskon: 15
 Output: harga yang harus dibayar adalah Rp. 314.500
 '''
+harga_awal = 370000
+diskon = 15/100
+
+harga_diskon = 15/100*harga_awal
+harga_akhir = harga_awal - harga_diskon
+
+print (f'harga yang harus dibayar adalah :{harga_akhir}')
